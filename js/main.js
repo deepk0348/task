8380c62d9710cd7fef20c49e0bf3d0d245329c0a
DEEPAK SHRESTHA <DEEPAK SHRESTHA>
2017-09-07 09:25:43 +0545

    task

---
+++ js/main.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function() {
+	jQuery('.bxslider').bxSlider();
+	jQuery('.reviews-slider').bxSlider({
+	 minSlides: 1,
+	 maxSlides: 2,
+	  slideWidth: 500,
+  slideMargin: 30
+	});
+});
